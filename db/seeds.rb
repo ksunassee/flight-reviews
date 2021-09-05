7a8fab07a8fdb6d30a22bc34597d59110cee14c2
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-09-05 15:19:25 +0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+airlines = Airline.create([
+	{
+		name: "Air Mauritius",
+		image_url: "https://res.cloudinary.com/dqmgxbzds/image/upload/v1630831821/Airlines%20logo/airmauritius_vxcmcf.jpg"
+	},
+	{
+		name: "Emirates",
+		image_url: "https://res.cloudinary.com/dqmgxbzds/image/upload/v1630831821/Airlines%20logo/emirates_w9v3c3.png"
+	},
+	{
+		name: "Air France",
+		image_url: "https://res.cloudinary.com/dqmgxbzds/image/upload/v1630831821/Airlines%20logo/air-france_efbq4w.jpg"
+	},
+	{
+		name: "British Airways"
+		image_url: "https://res.cloudinary.com/dqmgxbzds/image/upload/v1630831821/Airlines%20logo/British-airways_ww3o4a.jpg"
+	},
+	{
+		name: "Vietnam Airline"
+		image_url: "https://res.cloudinary.com/dqmgxbzds/image/upload/v1630831821/Airlines%20logo/vn-airline_tcwtwv.jpg"
+	}
+])
+
+reviews = Review.create([
+	{
+		title: "Great flight",
+		description: "I had a lovely time.",
+		score: 5,
+		airline: airlines.first
+	},
+	{
+		title: "Bad airline",
+		description: "I had a bad time",
+		score: 1,
+		airline: airlines.first
+	}
+
+])
